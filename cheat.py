856d1f3c4d0e9855f3dd26a9e608cc720e6b1169
Piyush Agarwal <piyushagarwal.0108@gmail.com>
2019-04-14 16:55:42 +0530

    new dialogues and randomisation

--- cheat.py
+++ cheat.py
@@ -1,8 +1,11 @@
 import text_to_speech as say
 import sys
+import random
 def safe(Girl,Boy):
+    dialogues = ["best friends always together","bhaiya mere rakhi ke bandhan ko nibhana","yeee dosti..hum nahi todenge","born rivals","both hates each other","kumbh ke mele main bichde hue bhai bhen"]
     if(Girl == "harshita sharma" and Boy != "piyush agarwal"):
-        print("best friends always together")
-        say.speak("best friends always together")
+        dialogue = random.choice(dialogues)
+        print(dialogue)
+        say.speak(dialogue)
         sys.exit()
         
